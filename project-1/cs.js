662f4e287bec852c156327009b326f13e95a6e3d
Datensh1 <163721995+Datensh1@users.noreply.github.com>
2024-03-18 20:07:09 +0800

    Add files via upload

---
+++ project-1/cs.js
@@ -0,0 +1,16 @@
+  
+   const button = document.getElementById("btn-1")
+   button.addEventListener("click", color)
+  function color(){
+  
+  var color = new Array('red', 'orange','yellow','green','blue','indigo','violet')
+button.addEventListener("click", () =>{ 
+document.querySelector('body').style.background = color[Math.floor(Math.random()*color.length)]} )
+ console.log("hello")
+  }
+ 
+  
+
+
+
+
